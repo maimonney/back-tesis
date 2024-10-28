6c481d04a4beaff8df241ef96b04d3cfe7b48e91
Mailen <maimonney30@gmail.com>
2024-10-28 17:23:39 -0300

    pruebas

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const vuelosController = require('../controllers/vuelosController'); // Ajusta la ruta según tu estructura
+const vuelosController = require('../controllers/vuelosControllers'); 
 
 const router = express.Router();
 
 router.get('/vuelos', vuelosController.obtenerVuelos);
-router.get('/vuelos/buscar', vuelosController.buscarVuelosIda);
-router.get('/vuelos/:id', vuelosController.obtenerVuelosId);
-router.get('/vuelos/destino/:destino', vuelosController.filtrarDestino);
-router.get('/vuelos/fecha/:fechaSalida', vuelosController.filtrarFechaSalida);
+// router.get('/vuelos/buscar', vuelosController.buscarVuelosIda);
+// router.get('/vuelos/:id', vuelosController.obtenerVuelosId);
+// router.get('/vuelos/destino/:destino', vuelosController.filtrarDestino);
+// router.get('/vuelos/fecha/:fechaSalida', vuelosController.filtrarFechaSalida);
 
 module.exports = (app) => {
     app.use('/arcana', router); // Asegúrate de que esto coincida con tus rutas

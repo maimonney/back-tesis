4a3c109d78af884ab0445416461e494dd919661c
Mailen <maimonney30@gmail.com>
2024-10-28 17:49:09 -0300

    pruebas vercel 10002

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
+const express = require('express');
 const userRouter = require('./userRouter');
 const vuelosRouter = require('./vuelosRouter');
 const reservasRouter = require('./reservasRouter');
 
-function routerAPI( app){
+const router = express.Router(); // Asegúrate de crear una instancia de Router
+const { obtenerVuelos } = require('../controllers/vuelosControllers');
+
+router.get('/', obtenerVuelos); // Esta ruta se encargará de obtener los vuelos
+
+// Configuración de las rutas
+function routerAPI(app) {
     app.use('/arcana/users', userRouter);
-    app.use('/arcana/vuelos', vuelosRouter);
-    app.use('/arcana/reservas',reservasRouter);
+    app.use('/arcana/vuelos', vuelosRouter); // Esta ruta usa el vuelosRouter
+    app.use('/arcana/reservas', reservasRouter);
 }
 
 module.exports = routerAPI;

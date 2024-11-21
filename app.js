47ccc091c8dacd6d55f8d641647b471070b79bce
Mailen <maimonney30@gmail.com>
2024-11-21 12:21:36 -0300

    cloudinary conectado

--- app.js
+++ app.js
@@ -23,15 +23,13 @@ const corsOptions = {
 api.use(cors(corsOptions));
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
     cloudinary_url: process.env.CLOUDINARY_URL,
   });
 
   const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
+        folder: 'lugares',
       allowed_formats: ['jpg', 'jpeg', 'png', 'gif'],
     },
   });

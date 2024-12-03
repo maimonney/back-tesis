7c43f86cab012ffa79d04daa5d47021a648a46f2
dayayalaa <day1414@hotmail.com>
2024-12-03 19:46:14 -0300

    jwt resuelto

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-<<<<<<< HEAD
-require('dotenv').config(); 
-
-=======
->>>>>>> af89e0012e0481f4e138b7b22371d7e7699ebe23
 // const cloudinaryUpload = require('./middleware/cloudinary');
 const routerAPI = require('./routes/index.js');
 
@@ -20,16 +15,15 @@ const apiVuelos = process.env.API_KEY;
 const corsOptions = {
     origin: ['http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-<<<<<<< HEAD
-    credentials: true 
-=======
+
+    credentials: true ,
+
     credentials: true,
->>>>>>> af89e0012e0481f4e138b7b22371d7e7699ebe23
+
 };
 
 // Middleware
 api.use(cors(corsOptions));
-<<<<<<< HEAD
 
 // Actualización: Elimina las opciones obsoletas
 mongoose.connect(process.env.MONGO_URI)
@@ -43,11 +37,11 @@ mongoose.connect(process.env.MONGO_URI)
 api.use(express.json());
 api.use(express.static('public'));
 // api.use('/upload', cloudinaryUpload.array('imagen')); 
-=======
+
 api.use(express.json());
 api.use(express.static('public'));
 // api.use('/upload', cloudinaryUpload.array('imagen'));
->>>>>>> af89e0012e0481f4e138b7b22371d7e7699ebe23
+
 
 // Conectar a MongoDB
 mongoose.connect(process.env.MONGO_URI)
@@ -72,12 +66,12 @@ api.use((err, req, res, next) => {
     res.status(500).json({ message: 'Error interno del servidor' });
 });
 
-<<<<<<< HEAD
+
 api.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 });
 
 module.exports = api; 
-=======
+
 module.exports = api;
->>>>>>> af89e0012e0481f4e138b7b22371d7e7699ebe23
+
--- routes/lugarRuta.js
+++ routes/lugarRuta.js
@@ -18,10 +18,9 @@ const {
 
 router.get('/', obtenerLugares);
 // router.post('/', upload.array('imagen'),  crearLugar);
-<<<<<<< HEAD
-=======
+
 router.post('/',  crearLugar);
->>>>>>> af89e0012e0481f4e138b7b22371d7e7699ebe23
+
 router.get('/:id', obtenerLugarId);
 router.delete('/:id', eliminarLugarId);
 router.put('/:id', actualizarLugarId);

cbad14c6c0e70cce3a24a5924563d965555eac20
Mailen <maimonney30@gmail.com>
2024-12-03 17:10:55 -0300

    actualizacion de app.js

--- app.js
+++ app.js
@@ -2,47 +2,49 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-require('dotenv').config(); 
+require('dotenv').config();
 
 // const cloudinaryUpload = require('./middleware/cloudinary');
 const routerAPI = require('./routes/index.js');
 
 const api = express();
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 const apiVuelos = process.env.API_KEY;
 
 const corsOptions = {
     origin: ['http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true 
-};;
+    credentials: true,
+};
 
+// Middleware
 api.use(cors(corsOptions));
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-      console.log('Conexión a MongoDB Atlas correcta');
-  })
-  .catch(err => {
-      console.error('Error al conectar a MongoDB Atlas:', err);
-  });
-
-
 api.use(express.json());
 api.use(express.static('public'));
-// api.use('/upload', cloudinaryUpload.array('imagen')); 
+// api.use('/upload', cloudinaryUpload.array('imagen'));
 
+// Conectar a MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Conexión a MongoDB Atlas correcta');
+        
+        // Inicia el servidor solo después de conectar a la base de datos
+        api.listen(port, () => {
+            console.log(`Servidor corriendo en el puerto ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error al conectar a MongoDB Atlas:', error.message);
+    });
+
+// Rutas
 routerAPI(api);
 
+// Manejo de errores
 api.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Error interno del servidor' });
-});
-
-api.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
 });
 
 module.exports = api;

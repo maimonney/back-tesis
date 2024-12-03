47134017f728752c04351f5ba24c0d523290698e
Mailen <maimonney30@gmail.com>
2024-12-03 17:05:42 -0300

    actualizacionde dependencias

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config(); 
 
-const cloudinaryUpload = require('./middleware/cloudinary');
+// const cloudinaryUpload = require('./middleware/cloudinary');
 const routerAPI = require('./routes/index.js');
 
 const api = express();
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 api.use(express.json());
 api.use(express.static('public'));
-api.use('/upload', cloudinaryUpload.array('imagen')); 
+// api.use('/upload', cloudinaryUpload.array('imagen')); 
 
 routerAPI(api);
 

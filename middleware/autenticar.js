4bc81a0bbfea833cb573782518113e0c005335a4
dayayalaa <day1414@hotmail.com>
2024-12-06 17:01:53 -0300

    jwt

--- middleware/autenticar.js
+++ middleware/autenticar.js
@@ -1,24 +1,31 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-dotenv.config();  
-const secreyKey = process.env.SECRETKEY; 
+dotenv.config();
+
+const secretKey = process.env.SECRETKEY;
 
 const autenticar = async (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1];  
+    try {
+        const token = req.headers.authorization; 
 
-    if (!token) {
-        return res.status(401).json({ message: 'No se pasó el JWT', data: [] });
-    }
-
-    // Verificamos el JWT
-    jwt.verify(token, secreyKey, (error, decoded) => {
-        if (error) {
-            return res.status(403).json({ message: 'JWT inválido', data: [] });
+      
+        if (!token) {
+            return res.status(401).json({ message: 'No se proporcionó un token', data: [] });
         }
 
-        req.user = decoded;  
-        next(); 
-    });
+        // Verificar el token
+        jwt.verify(token, secretKey, (error, decoded) => {
+            if (error) {
+                return res.status(403).json({ message: 'El token proporcionado es inválido o ha expirado', data: [] });
+            }
+
+            req.user = decoded; 
+            next(); 
+        });
+    } catch (error) {
+        console.error('Error en el middleware de autenticación:', error);
+        res.status(500).json({ message: 'Error interno en la autenticación', data: [] });
+    }
 };
 
 export { autenticar };

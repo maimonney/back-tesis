85e54c689a0f179531a4a299be4c92081a01a029
Mailen <maimonney30@gmail.com>
2025-01-15 17:30:11 -0300

    se agrego destino dentro de provincia

--- routes/serpApiRuta.js
+++ routes/serpApiRuta.js
@@ -1,4 +1,3 @@
-// serpApiRuta.js
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
@@ -33,7 +32,7 @@ router.get("/lugares", async (req, res) => {
     console.log("Respuesta de SerpAPI recibida:", response.data);
 
     if (response.data && response.data.place_results) {
-      console.log("Informacion encontrada:", response.data.place_results);
+      console.log("Información encontrada:", response.data.place_results);
       return res.json(response.data);
     } else {
       console.log("No se encontraron resultados para esta provincia.");
@@ -46,6 +45,56 @@ router.get("/lugares", async (req, res) => {
     console.error("Detalles del error:", error.response?.data);
     return res.status(500).json({
       error: "Hubo un problema al obtener los lugares",
+      message: error.message,
+      response: error.response?.data,
+      status: error.response?.status,
+    });
+  }
+});
+
+router.get("/destinos", async (req, res) => {
+  const { provincia } = req.query;
+
+  console.log("Recibida solicitud para obtener destinos, provincia:", provincia);
+
+  if (!provincia) {
+    console.log("Error: No se proporcionó provincia.");
+    return res
+      .status(400)
+      .json({ error: "Se debe proporcionar el nombre de la provincia" });
+  }
+
+  try {
+    const apiKey = process.env.SERP_API_KEY;
+
+    console.log("API Key:", apiKey);
+
+    const response = await axios.get("https://serpapi.com/search", {
+      params: {
+        engine: "google",
+        q: `${provincia} Destinations, Argentina`, 
+        location: "Argentina",
+        api_key: apiKey,
+        hl: "es"
+      },
+    });
+
+    console.log("Respuesta de SerpAPI para destinos recibida:", response.data);
+
+    if (response.data && response.data.local_results) {
+      console.log("Información de destinos encontrada:", response.data.local_results);
+      return res.json(response.data.local_results); 
+    } else {
+      console.log("No se encontraron destinos para esta provincia.");
+      return res
+        .status(404)
+        .json({ error: "No se encontraron destinos para esta provincia" });
+    }
+  } catch (error) {
+    console.error("Error al realizar la solicitud a SerpAPI para destinos:", error);
+    console.error("Detalles del error:", error.response?.data);
+    return res.status(500).json({
+      error: "Hubo un problema al obtener los destinos turísticos",
       message: error.message,
       response: error.response?.data,
       status: error.response?.status,

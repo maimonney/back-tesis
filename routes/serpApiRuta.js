4d3ba1dd0d73283eb9311141a097d65242ddcf61
Mailen <maimonney30@gmail.com>
2025-01-15 15:35:00 -0300

    prueba para incorporar serapi 10

--- routes/serpApiRuta.js
+++ routes/serpApiRuta.js
@@ -7,7 +7,10 @@ const router = express.Router();
 router.get("/lugares", async (req, res) => {
   const { provincia } = req.query;
 
+  console.log("Recibida solicitud para obtener lugares, provincia:", provincia);
+
   if (!provincia) {
+    console.log("Error: No se proporcionó provincia.");
     return res
       .status(400)
       .json({ error: "Se debe proporcionar el nombre de la provincia" });
@@ -15,6 +18,10 @@ router.get("/lugares", async (req, res) => {
 
   try {
     const apiKey = process.env.SERP_API_KEY;
+
+    // Verifica que la API Key esté correctamente configurada
+    console.log("API Key:", apiKey);
+
     const response = await axios.get("https://serpapi.com/search", {
       params: {
         engine: "google_maps",
@@ -23,7 +30,10 @@ router.get("/lugares", async (req, res) => {
       },
     });
 
+    console.log("Respuesta de SerpAPI recibida:", response.data);
+
     if (response.data && response.data.organic_results) {
+      console.log("Lugares encontrados:", response.data.organic_results);
       const lugares = response.data.organic_results.map((lugar) => ({
         nombre: lugar.title,
         url: lugar.link,
@@ -31,15 +41,14 @@ router.get("/lugares", async (req, res) => {
       }));
       return res.json(lugares);
     } else {
+      console.log("No se encontraron resultados para esta provincia.");
       return res
         .status(404)
         .json({ error: "No se encontraron lugares para esta provincia" });
     }
   } catch (error) {
-    console.error(
-      "Error al realizar la solicitud a SerpAPI:",
-      error.response?.data
-    );
+    console.error("Error al realizar la solicitud a SerpAPI:", error);
+    console.error("Detalles del error:", error.response?.data);
     return res.status(500).json({
       error: "Hubo un problema al obtener los lugares",
       message: error.message,

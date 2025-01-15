d469e5d21edd1fa9e95c39a0a5e6c497ff6d3694
Mailen <maimonney30@gmail.com>
2025-01-15 15:27:53 -0300

    prueba para incorporar serapi 09

--- routes/serpApiRuta.js
+++ routes/serpApiRuta.js
@@ -1,21 +1,23 @@
 // serpApiRuta.js
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
 // Ruta para obtener los lugares de una provincia
-router.get('/lugares', async (req, res) => {
+router.get("/lugares", async (req, res) => {
   const { provincia } = req.query;
 
   if (!provincia) {
-    return res.status(400).json({ error: 'Se debe proporcionar el nombre de la provincia' });
+    return res
+      .status(400)
+      .json({ error: "Se debe proporcionar el nombre de la provincia" });
   }
 
   try {
-    const apiKey = process.env.SERP_API_KEY; 
-    const response = await axios.get('https://serpapi.com/search', {
+    const apiKey = process.env.SERP_API_KEY;
+    const response = await axios.get("https://serpapi.com/search", {
       params: {
-        engine: 'google_maps',
+        engine: "google_maps",
         q: provincia,
         api_key: apiKey,
       },
@@ -29,12 +31,17 @@ router.get('/lugares', async (req, res) => {
       }));
       return res.json(lugares);
     } else {
-      return res.status(404).json({ error: 'No se encontraron lugares para esta provincia' });
+      return res
+        .status(404)
+        .json({ error: "No se encontraron lugares para esta provincia" });
     }
   } catch (error) {
-    console.error('Error al realizar la solicitud a SerpAPI:', error);
+    console.error(
+      "Error al realizar la solicitud a SerpAPI:",
+      error.response?.data
+    );
     return res.status(500).json({
-      error: 'Hubo un problema al obtener los lugares',
+      error: "Hubo un problema al obtener los lugares",
       message: error.message,
       response: error.response?.data,
       status: error.response?.status,

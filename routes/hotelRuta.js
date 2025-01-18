b06d1afb4d5e60f5dd0d44544e98bacb0cd5a0e3
Mailen <maimonney30@gmail.com>
2025-01-18 15:45:25 -0300

    pequeña modificacion

--- routes/hotelRuta.js
+++ routes/hotelRuta.js
@@ -2,25 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { 
-    crearHotel,
     obtenerHotel,
-    obtenerHotelId,
-    habitacionesDestino,
-    actualizarHotelId,
-    eliminarHotel,
-    hotelEconomico,
-    obtenerHabitacion,
-    obtenerHabitacionId
 } = require('../controllers/hotelControlador');
 
 router.get('/', obtenerHotel);
-router.post('/', crearHotel);
-router.get('/:id', obtenerHotelId);
-router.get('/destino', habitacionesDestino);
-router.delete('/:id', eliminarHotel);
-router.put('/:id', actualizarHotelId);
-router.get('/precio/economico', hotelEconomico);
-router.get('/buscar/habitaciones', obtenerHabitacion);
-router.get('/:id/habitaciones', obtenerHabitacionId);
 
 module.exports = router;

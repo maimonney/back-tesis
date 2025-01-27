1ea0705dedea1011dd8a8835c44c6053943d8660
dayayalaa <day1414@hotmail.com>
2025-01-27 18:30:59 -0300

    prueba 01 Reserva para guias

--- models/reservasModelo.js
+++ models/reservasModelo.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const reservaSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-  vueloIda: { type: Object, required: true },
-  vueloVuelta: { type: [Object], required: true },
-  hotel: { type: Object, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    vueloIda: { type: Object, required: true },
+    vueloVuelta: { type: [Object], required: true },
+    hotel: { type: Object, required: true },
+    excursionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Excursion', required: false } // Nuevo campo
 });
 
 const Reserva = mongoose.model('Reserva', reservaSchema);

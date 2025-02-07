e562cf12c929ebd17f008e262045dfdd3cdc2409
dayayalaa <day1414@hotmail.com>
2025-02-07 20:10:56 -0300

    se agrego un pequeño cambio

--- routes/reservaTourRuta.js
+++ routes/reservaTourRuta.js
@@ -10,19 +10,12 @@ const {
     obtenerReservasPorGuia,
 } = require('../controllers/reservasTourControlador');
 
-// Crear una reserva de tour
 router.post('/tours', crearReservaTur);
-
-// Obtener todas las reservas de tours
 router.get('/tours', obtenerReservasTur);
-
-// Obtener una reserva de tour por su ID
 router.get('/tours/:id', obtenerReservaTurPorId);
-
-// Cancelar una reserva de tour
 router.put('/tours/:id/cancelar', cancelarReservaTur);
 
-// Obtener reservas de tours por guía
+// Obtiene reservas de tours por guía
 router.get('/tours/guia/:guiaId', obtenerReservasPorGuia);
 
 module.exports = router;

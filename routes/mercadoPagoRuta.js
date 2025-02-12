0582845fa6d43e4c4466425b7761a9bbccc7518a
Mailen <maimonney30@gmail.com>
2025-02-12 20:37:25 -0300

    se modifico mercado

--- routes/mercadoPagoRuta.js
+++ routes/mercadoPagoRuta.js
@@ -11,21 +11,34 @@ const client = new MercadoPagoConfig({
 router.post('/mercado', async (req, res) => {
     const { items } = req.body;
 
+    console.log('Cuerpo de la solicitud:', req.body);
+
+    console.log('Token de acceso:', process.env.MERCADOPAGO_ACCESS_TOKEN);
+
     const preference = new Preference(client);
-    preference.items = items.map(item => ({
-        title: item.title,
-        unit_price: item.price,
-        quantity: item.quantity,
-    }));
+    preference.items = items.map(item => {
+        console.log('Procesando item:', item);
+        return {
+            title: item.title,
+            unit_price: item.price,
+            quantity: item.quantity,
+        };
+    });
+
     preference.back_urls = {
         success: 'https://back-tesis-lovat.vercel.app/success',
         failure: 'https://back-tesis-lovat.vercel.app/failure',
         pending: 'https://back-tesis-lovat.vercel.app/pending',
     };
+    
     preference.auto_return = 'approved';
 
     try {
+        console.log('Creando preferencia...');
         const response = await preference.create();
+
+        console.log('Respuesta de Mercado Pago:', response.body);
+
         res.json({
             init_point: response.body.init_point,
         });
@@ -35,18 +48,18 @@ router.post('/mercado', async (req, res) => {
     }
 });
 
-// Ruta de éxito
 router.get('/success', (req, res) => {
+    console.log('Pago realizado con éxito');
     res.send('Pago realizado con éxito');
 });
 
-// Ruta de falla
 router.get('/failure', (req, res) => {
+    console.log('El pago ha fallado');
     res.send('El pago ha fallado');
 });
 
-// Ruta pendiente
 router.get('/pending', (req, res) => {
+    console.log('El pago está pendiente');
     res.send('El pago está pendiente');
 });
 

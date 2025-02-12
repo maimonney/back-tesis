121ad77e60a3d50d43d37ebc263777b0b6fb1b68
Mailen <maimonney30@gmail.com>
2025-02-12 18:58:06 -0300

    cambio en el cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const api = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: '*',
+    origin: ['https://back-tesis-lovat.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true, 

f8a16d526a76f4d002d15c86ef13edd0d99ebea7
Mailen <maimonney30@gmail.com>
2025-02-12 20:50:23 -0300

    se modifico mercado 06

--- routes/mercadoPagoRuta.js
+++ routes/mercadoPagoRuta.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { MercadoPagoConfig, Preference } = require('mercadopago');
+const express = require("express");
+const { MercadoPagoConfig, Preference } = require("mercadopago");
 
 const router = express.Router();
 
@@ -8,68 +8,77 @@ const client = new MercadoPagoConfig({
   public_key: process.env.MERCADOPAGO_PUBLIC_KEY,
 });
 
-router.post('/mercado', async (req, res) => {
-    const { items } = req.body;
+router.post("/mercado", async (req, res) => {
+  const { items } = req.body;
 
-    console.log('Cuerpo de la solicitud:', req.body);
-    console.log('Cuerpo', items);
+  console.log("Cuerpo de la solicitud:", req.body);
+  console.log("Cuerpo", items);
 
-    const preference = new Preference(client);
-    preference.items = items.map(item => {
-        console.log('Procesando item:', item);
-        return {
-            title: item.title,
-            unit_price: item.price,
-            quantity: item.quantity,
-        };
-    });
-
-    preference.back_urls = {
-        success: 'https://back-tesis-lovat.vercel.app/success',
-        failure: 'https://back-tesis-lovat.vercel.app/failure',
-        pending: 'https://back-tesis-lovat.vercel.app/pending',
+  const preference = new Preference(client);
+  preference.items = items.map((item) => {
+    console.log("Procesando item:", item);
+    return {
+      title: item.title,
+      unit_price: item.price,
+      quantity: item.quantity,
     };
-    
-    preference.auto_return = 'approved';
+  });
 
-    console.log('Datos de la preferencia:', preference);
+  preference.back_urls = {
+    success: "https://back-tesis-lovat.vercel.app/success",
+    failure: "https://back-tesis-lovat.vercel.app/failure",
+    pending: "https://back-tesis-lovat.vercel.app/pending",
+  };
 
-    try {
-        console.log('Creando preferencia...');
-        const response = await preference.create();
+  preference.auto_return = "approved";
 
-        console.log('Body:', body);
-        console.log('Body:', response.body);
-    
-        console.log('Respuesta completa de Mercado Pago:', response);
-    
-        if (response && response.body) {
-            res.json({
-                init_point: response.body.init_point,
-            });
-        } else {
-            console.error('La respuesta no tiene el cuerpo esperado:', response);
-            res.status(500).json({ error: 'No se obtuvo una respuesta válida de Mercado Pago' });
-        }
-    } catch (error) {
-        console.error('Error al crear la preferencia:', error);
-        res.status(500).json({ error: 'Error al crear la preferencia' });
+  console.log("Datos de la preferencia:", preference);
+
+  try {
+    console.log("Creando preferencia...");
+
+    console.log("Body:", body);
+
+    const response = await preference.create();
+
+    console.log("Respuesta completa de Mercado Pago:", response);
+
+    if (response && response.body) {
+      console.log("Respuesta completa de Mercado Pago:", response);
+      res.json({
+        init_point: response.body.init_point,
+      });
+    } else {
+      console.error("La respuesta no contiene un cuerpo válido:", response);
+      res
+        .status(500)
+        .json({ error: "No se obtuvo una respuesta válida de Mercado Pago" });
     }
+  } catch (error) {
+    console.error("Error al crear la preferencia:", error.message);
+    console.error(
+      "Detalles del error:",
+      error.response ? error.response.data : "No hay detalles"
+    );
+    res
+      .status(500)
+      .json({ error: "Error al crear la preferencia", details: error.message });
+  }
 });
 
-router.get('/success', (req, res) => {
-    console.log('Pago realizado con éxito');
-    res.send('Pago realizado con éxito');
+router.get("/success", (req, res) => {
+  console.log("Pago realizado con éxito");
+  res.send("Pago realizado con éxito");
 });
 
-router.get('/failure', (req, res) => {
-    console.log('El pago ha fallado');
-    res.send('El pago ha fallado');
+router.get("/failure", (req, res) => {
+  console.log("El pago ha fallado");
+  res.send("El pago ha fallado");
 });
 
-router.get('/pending', (req, res) => {
-    console.log('El pago está pendiente');
-    res.send('El pago está pendiente');
+router.get("/pending", (req, res) => {
+  console.log("El pago está pendiente");
+  res.send("El pago está pendiente");
 });
 
 module.exports = router;

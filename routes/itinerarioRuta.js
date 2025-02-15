2257fcbe5a38b7d10d67c8e5571374487de6ed6d
Mailen <maimonney30@gmail.com>
2025-02-15 16:03:43 -0300

    cambio en la key de serpapi

--- routes/itinerarioRuta.js
+++ routes/itinerarioRuta.js
@@ -13,17 +13,14 @@ const {
     eliminarItem,
 } = require('../controllers/itinerarioControlador');
 
-// Rutas para obtener información
 router.get('/usuario/:userId', obtenerReservaUserId);
 router.get('/destino', obtenerReservaDestino);
 router.get('/:id', obtenerItinerarioId);
 
-// Rutas para crear, actualizar y borrar
 router.post('/crear', crearReserva);
 router.delete('/:id', borrarReserva);
 router.put('/:id', actualizarReserva);
 
-// Rutas para manejar los ítems del checklist
 router.post('/:id/checklist/agregar', agregarItem); 
 router.put('/:id/checklist/actualizar', actualizarItem); 
 router.delete('/:id/checklist/eliminar', eliminarItem); 

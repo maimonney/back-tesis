e3828ee75fdfdafe41e03b49aa525428dcaa53c9
Mailen <maimonney30@gmail.com>
2025-02-22 15:06:27 -0300

    se agrego el mail del viaje

--- routes/mailRuta.js
+++ routes/mailRuta.js
@@ -2,9 +2,9 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const reservas = require('../models/reservaTourModelo.js'); // Reserva de tour
-const usuarios = require('../models/usuarioModelo.js'); // Información de los usuarios y guías
-
+const reservas = require("../models/reservaTourModelo.js");
+const usuarios = require("../models/usuarioModelo.js");
+const itinerario = require("../models/itinerarioModelo.js");
 
 const router = express.Router();
 const passMail = process.env.CLAVE_MAIL;
@@ -24,34 +24,46 @@ const transporter = nodemailer.createTransport({
 
 router.post("/reserva", async (req, res) => {
   try {
-    console.log('Datos recibidos:', req.body);
+    console.log("Datos recibidos:", req.body);
 
     const { usuarioEmail, guiaEmail, reserva } = req.body;
-    const { userId, tourId, cantidadPersonas, fechaTour, destino, precio } = reserva;
+    const { userId, tourId, cantidadPersonas, fechaTour, destino, precio } =
+      reserva;
 
-    if (!usuarioEmail || !guiaEmail || !userId || !tourId || !cantidadPersonas || !fechaTour || !destino || !precio) {
-      return res.status(400).json({ message: "Faltan datos requeridos para enviar el correo." });
+    if (
+      !usuarioEmail ||
+      !guiaEmail ||
+      !userId ||
+      !tourId ||
+      !cantidadPersonas ||
+      !fechaTour ||
+      !destino ||
+      !precio
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Faltan datos requeridos para enviar el correo." });
     }
 
     const usuario = await usuarios.findById(userId);
-    const nombreUsuario = usuario ? usuario.nombre : 'Usuario desconocido'; 
-    
+    const nombreUsuario = usuario ? usuario.nombre : "Usuario desconocido";
+
     const tour = await reservas.findById(tourId);
 
     console.log(tour);
 
-    const tourTitulo = tour ? tour.titulo : 'Tour desconocido'; 
-    
-    const fechaFormateada = new Date(fechaTour).toLocaleDateString('es-AR', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
+    const tourTitulo = tour ? tour.titulo : "Tour desconocido";
+
+    const fechaFormateada = new Date(fechaTour).toLocaleDateString("es-AR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
     });
 
     const mailOptionsUsuario = {
-      from: 'mailen.monney@davinci.edu.ar',
+      from: "mailen.monney@davinci.edu.ar",
       to: usuarioEmail,
-      subject: 'Confirmación de Reserva',
+      subject: "Confirmación de Reserva",
       html: `
         <div style="font-family: Arial, sans-serif; color: #333; background-color: #f9fafb; padding: 20px;">
           <div style="background-color: #ffffff; border-radius: 8px; padding: 20px; max-width: 600px; margin: 0 auto; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);">
@@ -79,9 +91,9 @@ router.post("/reserva", async (req, res) => {
     };
 
     const mailOptionsGuia = {
-      from: 'mailen.monney@davinci.edu.ar',
+      from: "mailen.monney@davinci.edu.ar",
       to: guiaEmail,
-      subject: 'Nueva Reserva',
+      subject: "Nueva Reserva",
       html: `
         <div style="font-family: Arial, sans-serif; color: #333; background-color: #f9fafb; padding: 20px;">
           <div style="background-color: #ffffff; border-radius: 8px; padding: 20px; max-width: 600px; margin: 0 auto; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);">
@@ -114,10 +126,109 @@ router.post("/reserva", async (req, res) => {
     await transporter.sendMail(mailOptionsGuia);
 
     res.status(200).json({ message: "Correo enviado con éxito." });
-
   } catch (error) {
-    console.error('Error al enviar el correo:', error);
-    res.status(500).json({ message: "Hubo un error al enviar el correo.", error: error.message });
+    console.error("Error al enviar el correo:", error);
+    res
+      .status(500)
+      .json({
+        message: "Hubo un error al enviar el correo.",
+        error: error.message,
+      });
+  }
+});
+
+router.post("/reservaViaje", async (req, res) => {
+  try {
+    const {
+      email,
+      name,
+      destino,
+      salida,
+      aerolinea,
+      fechaIda,
+      fechaVuelta,
+      precioIda,
+      precioVuelta,
+      hotel,
+      fechaHotel,
+      precioHotel,
+      total,
+      itinerarioId,
+    } = req.body;
+
+    if (
+      !email || !name || !destino || !salida || !aerolinea || !fechaIda || !fechaVuelta || !precioIda || !precioVuelta || !hotel || !fechaHotel || !precioHotel || !total
+    ) {
+      return res.status(400).json({ error: "Faltan datos en la solicitud" });
+    }
+
+    const itinerarioData = await itinerario.findById(itinerarioId);
+
+    console.log("itinerario:", itinerarioData);
+
+    if (!itinerario) {
+      return res.status(404).json({ error: "Itinerario no encontrado" });
+    }
+
+    const htmlContent = `
+      <div style="font-family: Arial, sans-serif; color: #333; background-color: #f9fafb; padding: 20px;">
+        <div style="background-color: #ffffff; border-radius: 8px; padding: 20px; max-width: 600px; margin: 0 auto; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);">
+          <div style="background-color: #788a68; color: white; padding: 10px; text-align: center; border-radius: 8px 8px 0 0;">
+            <h1 style="margin: 0; font-size: 24px;">Confirmación de reserva de viaje</h1>
+          </div>
+          <div style="margin-top: 20px;">
+            <p style="text-align: center; font-size: 20px;">Hola <strong>${name}</strong></p>
+            <h2 style="color: #A86A36;">Tu reserva ha sido registrada:</h2>
+            <p><strong>Destino:</strong> ${destino}</p>
+            
+            <h2 style="color: #A86A36;">Detalles de la reserva</h2>
+
+            <h3 style="color: #788a68;">Información de ida</h3>
+            <ul>
+              <li><strong>Salida:</strong> ${salida}</li>
+              <li><strong>Aerolínea:</strong> ${aerolinea}</li>
+              <li><strong>Fecha:</strong> ${fechaIda}</li>
+              <li><strong>Precio:</strong> $${precioIda}</li>
+            </ul>
+
+            <h3 style="color: #788a68;">Información de vuelta</h3>
+            <ul>
+              <li><strong>Regreso desde:</strong> ${destino}</li>
+              <li><strong>Aerolínea:</strong> ${aerolinea}</li>
+              <li><strong>Fecha:</strong> ${fechaVuelta}</li>
+              <li><strong>Precio:</strong> $${precioVuelta}</li>
+            </ul>
+
+            <h3 style="color: #788a68;">Información del hotel</h3>
+            <ul>
+              <li><strong>Nombre:</strong> ${hotel}</li>
+              <li><strong>Fecha:</strong> ${fechaHotel}</li>
+              <li><strong>Precio:</strong> $${precioHotel}</li>
+            </ul>
+
+            <h3 style="color: #788a68;">Total pagado</h3>
+            <p><strong>Total:</strong> $${total}</p>
+
+            <p style="color: red; font-size: 12px;">*Para pagos y cancelaciones, contacta a la aerolínea o al hotel reservado.</p>
+          </div>
+
+          <div style="text-align: center; margin-top: 30px; font-size: 12px; color: #777;">
+            <hr>
+            <p>Gracias por reservar con nosotros.</p>
+          </div>
+        </div>
+      </div>
+    `;
+
+    await transporter.sendMail(htmlContent);
+    console.log("Email enviado:", info.response);
+
+    res.status(200).json({ message: "Correo enviado correctamente" });
+  } catch (error) {
+    console.error("Error al enviar el correo:", error);
+    res
+      .status(500)
+      .json({ error: "Error al enviar el correo", detalle: error.message });
   }
 });
 
